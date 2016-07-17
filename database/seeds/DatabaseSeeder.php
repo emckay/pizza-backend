2b5175353e0584c7484b22b1a8c71214e8a5d39f
Eric <ericmckay4@gmail.com>
2016-07-17 00:25:55 -0700

    db seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\PizzaStatus;
+use App\Topping;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        PizzaStatus::firstOrCreate(['name' => 'Pending', 'order' => 0]);
+        PizzaStatus::firstOrCreate(['name' => 'Submitted', 'order' => 1]);
+        PizzaStatus::firstOrCreate(['name' => 'Ready', 'order' => 2]);
+
+        $toppingList = array('Pepperoni', 'Sausage', 'Mushrooms', 'Olives', 'Spinach');
+
+        foreach($toppingList as $name)
+        {
+            Topping::firstOrCreate(['name' => $name]);
+        }
     }
 }
