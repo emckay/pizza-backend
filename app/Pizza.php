c9b081b84dc4260e5718ae2b82e84c4441a7b545
Eric <ericmckay4@gmail.com>
2016-07-16 23:55:47 -0700

    parameter whitelist and default status for pizza model

--- app/Pizza.php
+++ app/Pizza.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pizza extends Model
 {
-    //
+    // TODO: Change this to default status rather than 0
+    protected $attributes = [
+        'pizza_status_id' => 0,
+    ];
+
+    protected $fillable = ['name'];
 }
